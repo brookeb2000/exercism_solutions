0c3105a2076b5e87f0967c9fc74ae293f09cbbce
unknown <brockerbrooke416@gmail.com>
2025-08-12 20:14:41 -0400

    update steps() function to take out unecessary step of declaring local variable with vale of number paramater

--- Collatz_Conjecture/collatz_conjecture.py
+++ Collatz_Conjecture/collatz_conjecture.py
@@ -1,13 +1,17 @@
 def steps(number):
+    """
+    takes a positive integer and returns the number if steps it takes to reach 1 according to the rules of the Collatz Conjecture
+    param number: int, positive integer greater than 0
+    return: int, number of steps takes to reach 1
+    """
     if number <= 0:
         raise ValueError("Only positive integers are allowed")
 
-    num = number
     steps = 0
-    while num != 1:
-        if num % 2 == 0: #if the number is even
-            num = num/2
+    while number != 1:
+        if number % 2 == 0: #if the number is even
+            number = number/2
         else:
-            num = num * 3 + 1
+            number = number * 3 + 1
         steps += 1
     return steps
