a5f8cae6b318cdc80222cb5a9b460148353cf559
unknown <brockerbrooke416@gmail.com>
2025-08-12 20:12:49 -0400

    add implementation of steps() method which returns the number of steps needed to get a positive integer to 1 based on the rules of the collatz conjecture

--- Collatz_Conjecture/collatz_conjecture.py
+++ Collatz_Conjecture/collatz_conjecture.py
@@ -1,2 +1,13 @@
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+
+    num = number
+    steps = 0
+    while num != 1:
+        if num % 2 == 0: #if the number is even
+            num = num/2
+        else:
+            num = num * 3 + 1
+        steps += 1
+    return steps
